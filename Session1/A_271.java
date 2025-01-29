ab0cbdc4150cb10349eec3cc6dc503c62591ab22
Simran <ss1043422@gmail.com>
2025-01-29 12:46:59 +0530

    Session1 CP

---
+++ Session1/A_271.java
@@ -0,0 +1,32 @@
+package Session1;
+
+import java.util.*;
+
+public class A_271 {
+	public static void distinct (int val) {
+		val=val+1;
+		while(true) {
+		    String s = Integer.toString(val);
+		    boolean available = true;
+		    HashSet<Character> set = new HashSet<>();
+		    for(char i :s.toCharArray()) {
+			    if(set.contains(i)) {
+				    available=false;
+				    break;
+			    }
+			    set.add(i);
+		    }
+		    if(available) {
+			    System.out.println(val);
+			    break;
+		    }val++;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		distinct(year);
+	}
+
+}
---
+++ Session1/A_381.java
@@ -0,0 +1,38 @@
+package Session1;
+
+import java.util.Scanner;
+
+public class A_381 {
+	public static void sum(int[] arr , int i, int j) {
+		int s=0; int d=0;boolean sTurn=true;
+		 while (i <= j) {
+	            if (arr[i] > arr[j]) {
+	                if (sTurn) {
+	                    s += arr[i];
+	                } else {
+	                    d += arr[i];
+	                }
+	                i++;
+	            } else {
+	                if (sTurn) {
+	                    s += arr[j];
+	                } else {
+	                    d += arr[j];
+	                }
+	                j--;
+	            }
+	            sTurn = !sTurn;
+	        }
+	        System.out.println(s + " " + d);
+	    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			 arr[i]= sc.nextInt();
+		}sum(arr,0,arr.length-1);
+
+	}
+
+}
